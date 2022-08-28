658427df50fc64738c4e28a27d15cb1a96c850c9
Isai Martinez <isai.martinez@yeico.com>
2022-08-28 00:12:16 -0500

    Utils

--- reactjs/tiktok_app/pages/index.tsx
+++ reactjs/tiktok_app/pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      Tik Tik
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   )
 }
